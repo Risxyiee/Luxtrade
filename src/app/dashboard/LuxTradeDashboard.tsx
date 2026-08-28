c67c227f2ddad219cfbf05d918fcf4077175f88e
Risxyie <riskiakbarp123@gmail.com>
2026-08-28 11:23:03 +0000

    51f85aba-ec83-4066-b02f-e137536cdebb

--- src/app/dashboard/LuxTradeDashboard.tsx
+++ src/app/dashboard/LuxTradeDashboard.tsx
@@ -26,7 +26,7 @@ import Sidebar from './components/Sidebar'
 import Header from './components/Header'
 import DashboardModals from './components/DashboardModals'
 import TabContent from './components/TabContent'
-import DashboardFAB from './components/DashboardFAB'
+
 
 // Extracted Utils & Hooks
 import type { Trade, JournalEntry, WatchlistItem, Analytics } from '@/types'
@@ -886,17 +886,6 @@ function LuxTradeDashboardContent() {
           tradingAccounts={tradingAccounts}
         />
       </main>
-
-      {/* Floating Action Button — Add Trade & Account */}
-      <DashboardFAB
-        language={language}
-        tradingAccounts={tradingAccounts}
-        selectedAccountId={selectedAccountId}
-        setSelectedAccountId={setSelectedAccountId}
-        setAddAccountOpen={setIsAddAccountOpen}
-        setAddTradeOpen={setAddTradeOpen}
-        fetchData={fetchData}
-      />
 
       {/* All Modals */}
       <DashboardModals
